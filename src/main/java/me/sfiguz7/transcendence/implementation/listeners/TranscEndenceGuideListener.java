7015995d0d12f9207d56f174366eaabb2d679eda
baoad <3200961931@QQ.com>
2021-08-21 22:54:58 +0800

    TranscEndence  zh-CN

--- src/main/java/me/sfiguz7/transcendence/implementation/listeners/TranscEndenceGuideListener.java
+++ src/main/java/me/sfiguz7/transcendence/implementation/listeners/TranscEndenceGuideListener.java
@@ -33,7 +33,7 @@ public class TranscEndenceGuideListener implements Listener {
         List<String> pages = new ArrayList<>();
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta meta = (BookMeta) book.getItemMeta();
-        meta.setTitle("末地科技书");
+        meta.setTitle("末地科技指南书");
         meta.setAuthor("作者:Sfiguz7 汉化:baoad");
         //Page 3 has an extra space after "Polarizer" otherwise "in" gets cut
         pages.add(ChatColors.color("你好!这是关于末地科技的指南.\n\n" +
@@ -41,10 +41,10 @@ public class TranscEndenceGuideListener implements Listener {
             "&r(只能在末地使用!)."));
         pages.add(ChatColors.color("从粒子生产机那你会得到4种不同的粒子&9如: &rup, down水平粒子, left, " +
             "right垂直粒子.\n\n" +
-            "你可以让粒子生产机产生水平和垂直粒子" +
+            "你可以让粒子生产机产生水平粒子和垂直粒子." +
             "通过两种不同的&d偏振器&r来生产对应的粒子几率."));
         pages.add(ChatColors.color("&4不稳定的锭和其他材料:" +
-            "小心点,如果拿住他超过了时间,那么他会&c杀了&r你,物品会永远的消失.\n\n" +
+            "小心点,如果拿住他超过了一定时间,那么他会&c杀了&r你,物品并且会永远的消失.\n\n" +
             "建议你用货运来运输."));
         pages.add(ChatColors.color("&a粒子过载机&r在里面放入未充能的粒子" +
             "用来给粒子充能.\n\n" +
@@ -57,9 +57,9 @@ public class TranscEndenceGuideListener implements Listener {
             "(F) - 抗性提升 4\n" +
             "(H) - 饱和\n" +
             "(R) - 生命恢复 2\n"));
-        pages.add(ChatColors.color("死亡的时候,所有的效果将会前功尽弃" +
-            "这意味着只能从头开始" +
-            "都要重新制作一个超能力核心"));
+        pages.add(ChatColors.color("死亡的时候,所有的超能力效果将会消失." +
+            "这意味着只能从头开始." +
+            "只能重新制作超能力核心."));
         meta.setPages(pages);
         book.setItemMeta(meta);
         return book;
